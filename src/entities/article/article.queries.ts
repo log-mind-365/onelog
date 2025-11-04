49da27fbc5d93c69fb32cbbdf7309b2fc201710d
kidboi666 <kidboi666@gmail.com>
2025-11-04 14:48:36 +0900

    refactor: POST_QUERY_KEY 제거 및 QUERY_KEY로 대체
    - `article.model.ts`에서 `POST_QUERY_KEY` 제거
    - `QUERY_KEY`를 사용하는 방식으로 `article.queries.ts` 업데이트
    - 코드 간소화 및 키 정의 위치 정리

--- src/entities/article/article.queries.ts
+++ src/entities/article/article.queries.ts
@@ -1,14 +1,12 @@
 import { infiniteQueryOptions } from "@tanstack/react-query";
 import { getInfinitePublicArticleList } from "@/entities/article/article.api";
-import {
-  type InfiniteArticleList,
-  POST_QUERY_KEY,
-} from "@/entities/article/article.model";
+import type { InfiniteArticleList } from "@/entities/article/article.model";
+import { QUERY_KEY } from "@/shared/model/constants";
 
 export const articleQueries = {
   infinite: () =>
     infiniteQueryOptions({
-      queryKey: POST_QUERY_KEY.PUBLIC,
+      queryKey: QUERY_KEY.ARTICLE.PUBLIC,
       queryFn: async ({ pageParam }): Promise<InfiniteArticleList> =>
         getInfinitePublicArticleList(pageParam),
       initialPageParam: "",
