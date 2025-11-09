0d26c355765b17fca93175284177182f4ba60db0
kidboi666 <kidboi666@gmail.com>
2025-11-09 10:55:36 +0900

    feat: storage 테이블에 avatars 관련 RLS 정책 추가

---
+++ src/db/policies/storage.sql
@@ -0,0 +1,27 @@
+CREATE policy "anyone can select avatars"
+ON storage.objects
+FOR
+SELECT
+TO PUBLIC
+USING (
+    bucket_id = 'avatars'
+);
+
+CREATE policy "authenticated can insert own avatars"
+ON storage.objects
+FOR INSERT
+TO authenticated
+WITH CHECK (
+  bucket_id = 'avatars'
+  AND (storage.foldername(NAME))[1] = auth.uid()::text
+  AND storage.extension(NAME) IN ('jpg', 'jpeg', 'png')
+);
+
+CREATE policy "authenticated can delete own avatars"
+ON storage.objects
+FOR DELETE
+TO authenticated
+USING (
+  bucket_id = 'avatars'
+  AND (storage.foldername(NAME))[1] = auth.uid()::text
+);
