a3c103dc9a1292957f708cd2cdef735c786b8963
kidboi666 <kidboi666@gmail.com>
2025-11-13 16:10:56 +0900

    refactor: write page를 Server Component로 변경
    - Client Component에서 Server Component로 변경
    - WritePageView로 로직 위임하여 관심사 분리

--- src/app/(home)/write/page.tsx
+++ src/app/(home)/write/page.tsx
@@ -1,53 +1,14 @@
-"use client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClient } from "@/shared/lib/tanstack/get-query-client";
+import { WritePageView } from "@/views/write/write-page-view";
 
-import type { ChangeEvent } from "react";
-import { ArticleHeader } from "@/entities/article/ui/article-header";
-import { ArticleForm } from "@/features/article/ui/article-form";
-import { useAuth } from "@/features/auth/model/store";
-import { PageContainer } from "@/shared/components/page-container";
-import { Card, CardContent, CardHeader } from "@/shared/components/ui/card";
-import { Separator } from "@/shared/components/ui/separator";
-import { useDraft } from "@/views/write/use-draft";
-import { WritePageHeader } from "@/views/write/write-page-header.widget";
-
-const Page = () => {
-  const { setTitle, setContent } = useDraft();
-  const title = useDraft((state) => state.title);
-  const emotionLevel = useDraft((state) => state.emotionLevel);
-  const content = useDraft((state) => state.content);
-  const { me } = useAuth();
-
-  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
-  };
-
-  const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(e.target.value);
-  };
+const Page = async () => {
+  const queryClient = getQueryClient();
 
   return (
-    <PageContainer title="게시글 작성" description="오늘은 어떤 일이 있었나요?">
-      <WritePageHeader />
-      <Card>
-        <CardHeader>
-          <ArticleHeader
-            userName={me?.userName ?? ""}
-            avatarUrl={me?.avatarUrl ?? ""}
-            email={me?.email ?? ""}
-            emotionLevel={emotionLevel}
-          />
-        </CardHeader>
-        <Separator />
-        <CardContent>
-          <ArticleForm
-            title={title}
-            content={content}
-            onContentChange={handleContentChange}
-            onTitleChange={handleTitleChange}
-          />
-        </CardContent>
-      </Card>
-    </PageContainer>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <WritePageView />
+    </HydrationBoundary>
   );
 };
 
