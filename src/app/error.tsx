3f21931d05bffbb14cfe32da157a10044c24bb2b
kidboi666 <kidboi666@gmail.com>
2025-12-03 17:03:46 +0900

    style: 에러 페이지 UI 버튼 스타일 통일

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import Link from "next/link";
+import {Button} from "@/shared/components/ui/button";
 
 // biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
 const Error = () => {
   return (
-    <div className="flex min-h-[calc(100vh-110px)] flex-col items-center justify-center">
-      <h2 className="mb-4 text-[20px] text-bold">
-        예상치 못한 오류가 발생했습니다.
-      </h2>
-      <Link
-        className="rounded-full bg-brand px-[20px] py-[10px] text-[15px] text-white"
-        href={"/"}
-      >
-        홈으로 이동하기
-      </Link>
-    </div>
+      <div className="flex min-h-[calc(100vh-110px)] flex-col items-center justify-center">
+        <h2 className="mb-4 text-[20px] text-bold">
+          예상치 못한 오류가 발생했습니다.
+        </h2>
+        <Button asChild>
+          <Link href={"/"}>
+            홈으로 이동하기
+          </Link>
+        </Button>
+      </div>
   );
 };
 
