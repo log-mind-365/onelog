094ca982b3345e28fb2ce9e05f106edd11ca00c8
kidboi666 <kidboi666@gmail.com>
2025-12-05 19:38:41 +0900

    feat(ui): 404 페이지 디자인 개선

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,8 @@
 
 import { ArrowUpRightIcon, TriangleAlertIcon } from "lucide-react";
 import Link from "next/link";
+import { BackButton } from "@/shared/components/back-button";
+import { PageContainer } from "@/shared/components/page-container";
 import { Button } from "@/shared/components/ui/button";
 import {
   Empty,
@@ -15,24 +17,29 @@ import { ROUTES } from "@/shared/model/routes";
 
 const NotFound = () => {
   return (
-    <Empty className="h-screen">
-      <EmptyHeader>
-        <EmptyMedia variant="icon">
-          <TriangleAlertIcon />
-        </EmptyMedia>
-        <EmptyTitle>404</EmptyTitle>
-        <EmptyDescription>
-          요청하신 페이지는 없는 페이지입니다.
-        </EmptyDescription>
-      </EmptyHeader>
-      <EmptyContent>
-        <Button variant="link" className="text-muted-foreground" asChild>
-          <Link href={ROUTES.HOME}>
-            홈으로 이동하기 <ArrowUpRightIcon />
-          </Link>
-        </Button>
-      </EmptyContent>
-    </Empty>
+    <div className="!mx-auto mt-18 w-full sm:mt-0 sm:w-lg md:w-xl lg:w-2xl">
+      <PageContainer>
+        <Empty className="relative border bg-card shadow-md">
+          <BackButton className="absolute top-4 left-4" />
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <TriangleAlertIcon />
+            </EmptyMedia>
+          </EmptyHeader>
+          <EmptyContent>
+            <EmptyTitle>404</EmptyTitle>
+            <EmptyDescription>
+              요청하신 페이지는 없는 페이지입니다.
+            </EmptyDescription>
+            <Button asChild>
+              <Link href={ROUTES.HOME}>
+                홈으로 이동하기 <ArrowUpRightIcon />
+              </Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </PageContainer>
+    </div>
   );
 };
 
